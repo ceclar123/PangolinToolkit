a11d6643442a64544a82d27dcfe0720d451d763f
ceclar123 <ceclar123@yeah.net>
2024-06-10 16:21:42 +0800

    json

---
+++ pangolin-tool/src/main/java/com/a/b/c/d/pangolin/tool/controller/json/FormatController.java
@@ -0,0 +1,85 @@
+package com.a.b.c.d.pangolin.tool.controller.json;
+
+import com.a.b.c.d.pangolin.tool.util.AlertUtil;
+import com.a.b.c.d.pangolin.util.ExceptionUtil;
+import com.a.b.c.d.pangolin.util.JsonUtil;
+import com.a.b.c.d.pangolin.util.StringUtil;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FormatController implements Initializable {
+    @FXML
+    private TextArea txtFrom;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    @FXML
+    public void btnFormatPrettyOnAction(ActionEvent event) {
+        String input = this.txtFrom.getText();
+        if (StringUtil.isBlank(input)) {
+            AlertUtil.showAlert(Alert.AlertType.INFORMATION, "提示", "输入内容为空");
+            return;
+        }
+
+        try {
+            this.txtFrom.setText(JsonUtil.formatPretty(input));
+        } catch (Exception e) {
+            AlertUtil.showAlert(Alert.AlertType.ERROR, "错误", e.getMessage(), ExceptionUtil.getStackTrace(e));
+        }
+    }
+
+    @FXML
+    public void btnFormatCompressOnAction(ActionEvent event) {
+        String input = this.txtFrom.getText();
+        if (StringUtil.isBlank(input)) {
+            AlertUtil.showAlert(Alert.AlertType.INFORMATION, "提示", "输入内容为空");
+            return;
+        }
+
+        try {
+            this.txtFrom.setText(JsonUtil.formatCompress(input));
+        } catch (Exception e) {
+            AlertUtil.showAlert(Alert.AlertType.ERROR, "错误", e.getMessage(), ExceptionUtil.getStackTrace(e));
+        }
+    }
+
+    @FXML
+    public void btnFormatAddEscapeOnAction(ActionEvent event) {
+        String input = this.txtFrom.getText();
+        if (StringUtil.isBlank(input)) {
+            AlertUtil.showAlert(Alert.AlertType.INFORMATION, "提示", "输入内容为空");
+            return;
+        }
+
+        try {
+            this.txtFrom.setText(JsonUtil.formatAddEscape(input));
+        } catch (Exception e) {
+            AlertUtil.showAlert(Alert.AlertType.ERROR, "错误", e.getMessage(), ExceptionUtil.getStackTrace(e));
+        }
+    }
+
+    @FXML
+    public void btnFormatRemoveEscapeOnAction(ActionEvent event) {
+        String input = this.txtFrom.getText();
+        if (StringUtil.isBlank(input)) {
+            AlertUtil.showAlert(Alert.AlertType.INFORMATION, "提示", "输入内容为空");
+            return;
+        }
+
+        try {
+            this.txtFrom.setText(JsonUtil.formatRemoveEscape(input));
+        } catch (Exception e) {
+            AlertUtil.showAlert(Alert.AlertType.ERROR, "错误", e.getMessage(), ExceptionUtil.getStackTrace(e));
+        }
+    }
+}
