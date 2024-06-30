b29715cdf576f9041468a0ccfc3b96248cd02286
ceclar123 <ceclar123@yeah.net>
2024-06-30 19:57:10 +0800

    图片

--- pangolin-tool/src/main/java/com/a/b/c/d/pangolin/tool/MainApplication.java
+++ pangolin-tool/src/main/java/com/a/b/c/d/pangolin/tool/MainApplication.java
@@ -1,6 +1,7 @@
 package com.a.b.c.d.pangolin.tool;
 
 import com.a.b.c.d.pangolin.tool.util.AlertUtil;
+import com.a.b.c.d.pangolin.util.ExceptionUtil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -12,8 +13,10 @@ import javafx.stage.Stage;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class MainApplication extends Application {
@@ -42,10 +45,20 @@ public class MainApplication extends Application {
 
         stage.show();
 
+
+        // 加载opencv本地库
+        String filePath = null;
         try {
-            System.load(MainApplication.class.getResource(getFilePath()).getFile());
-        } catch (Exception e) {
-            AlertUtil.showAlert(Alert.AlertType.ERROR, "错误", "加载opencv报错");
+            String fileName = getFileName();
+            filePath = MainApplication.class.getResource("/lib/" + fileName).getPath();
+            if (StringUtils.contains(filePath, "!BOOT-INF")) {
+                filePath = getNewFilePath(filePath);
+                System.load(filePath);
+            } else {
+                System.load(filePath);
+            }
+        } catch (Throwable e) {
+            AlertUtil.showAlert(Alert.AlertType.ERROR, "错误", "加载opencv报错:" + filePath, ExceptionUtil.getStackTrace(e));
         }
     }
 
@@ -60,16 +73,44 @@ public class MainApplication extends Application {
                 }).collect(Collectors.toList());
     }
 
-    private static String getFilePath() {
+    private static String getFileName() {
         String osName = StringUtils.lowerCase(System.getProperty("os.name"));
         if (osName.contains("win")) {
-            return "/lib/opencv_java490.dll";
+            return "opencv_java490.dll";
         } else if (osName.contains("linux")) {
-            return "/lib/libopencv_java4.so";
+            return "libopencv_java4.so";
         } else if (osName.contains("mac")) {
-            return "/lib/opencv_java490.dylib";
+            return "opencv_java490.dylib";
         } else {
             throw new UnsupportedOperationException();
         }
+    }
+
+    /**
+     * <pre>
+     *      输入:nested:/d:/a/b/PangolinToolkit/pangolin-tool-final.jar/!BOOT-INF/classes/!lib/opencv_java490.dll
+     *      输出:d:/a/b/PangolinToolkit/app/classes/lib/opencv_java490.dll
+     *  </pre>
+     *
+     * @param path
+     * @return
+     */
+    private static String getNewFilePath(String path) throws MalformedURLException {
+        path = StringUtils.replace(path, "nested:/", "");
+        String[] array = StringUtils.split(path, "/");
+        int len = array.length;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].endsWith(".jar")) {
+                index = i;
+                break;
+            }
+        }
+
+        path = Arrays.stream(array)
+                .filter(Objects::nonNull)
+                .limit(index - 1)
+                .collect(Collectors.joining("/")) + "/app/classes/lib/" + array[len - 1];
+        return path;
     }
 }
